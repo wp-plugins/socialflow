6ef638d8399a00d7b53cbd97bc2fb8c7868a49c0
stresslimit <stresslimit@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-11-04 23:23:47 +0000

    take edge stuff out of 0.1 tag
    git-svn-id: https://plugins.svn.wordpress.org/socialflow/trunk@459568 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/socialflow.js
+++ js/socialflow.js
@@ -1,4 +1,81 @@
 jQuery(function($){
+
+/* dashboard & shared functions */
+
+	$('.shorten-links').click(function() {
+		$('.ajax-loading').css('visibility','visible');
+
+		var sf_message = $('#sf-text').val();
+		var data = {
+			action: 'sf-shorten-msg',
+			sf_message: sf_message
+		};
+
+		$.post(ajaxurl, data, function(response) {
+			$('#sf-text').val(response);
+			$('#shorten-links #ajax-loading').css('visibility','hidden');
+			$('#sf-text').trigger('keyup');
+		});
+	});
+
+
+/* add/edit screen functions */
+
+	$('#title').keyup(function() {
+		//if ( 0 == $('#sf-text').val().length || $('#title').val().search( $('#sf-text').val() ) === 0 ) {
+		$('#sf-text').val( $('#title').val() );
+		$('#sf-text').trigger('keyup');
+		//}
+	});
+
+	$('#sf-text').maxlength({
+		events: [],				// Array of events to be triggerd    
+		maxCharacters: sf_l10n['max'],		// Characters limit   
+		statusID: "sf_char_count",
+		status: true,			// True to show status indicator bewlow the element    
+		statusClass: "",	// The class on the status div  
+		statusText: "", 					// The status text  
+		notificationClass: "notification",	// Will be added when maxlength is reached  
+		showAlert: false, // True to show a regular alert message    
+		alertText: "You have typed too many characters.", // Text in alert message   
+		slider: false // True Use counter slider    
+	});
+	
+
+/* post listing */
+
+
+
+	$('.row-actions .sf_publish a').click(function(e){
+		// e.preventDefault();
+		// return confirm('Are you sure you want to send this post to SocialFlow?');
+		var $the_a = $(this);
+		var the_title = prompt('Please enter the text of the post you want to send:', $the_a.parents('td').find('.row-title').html() );
+		if ( the_title != null && the_title != "" ) {
+			return true;
+			// TODO:
+			// $the_a.sibling('ajax-loading').css('visibility','visible');
+			// var sf_message = the_title;
+			// var data = {
+			// 	action: 'sf-send-msg',
+			// 	sf_message: sf_message
+			// };
+			// $.post(ajaxurl, data, function(response) {
+			// 	$the_a.sibling('ajax-loading').css('visibility','hidden');
+			// 	if ( '0'===response )
+			// 		$the_a.html('Sent to SocialFlow');
+			// 	else
+			// 		$the_a.html('There was an error');
+			// });
+		} else {
+			return false;
+		}
+	});
+
+
+
+/* configure settings functions */
+
 	$('.edit-sf-auth').click(function() {
 		$('#sf-auth-div').slideDown('fast');
 		$(this).hide();
@@ -72,41 +149,7 @@ jQuery(function($){
 		return false;
 	});
 
-	$('.shorten-links').click(function() {
-		$('.ajax-loading').css('visibility','visible');
 
-		var sf_message = $('#sf-text').val();
-		var data = {
-			action: 'sf-shorten-msg',
-			sf_message: sf_message
-		};
-
-		$.post(ajaxurl, data, function(response) {
-			$('#sf-text').val(response);
-			$('#shorten-links #ajax-loading').css('visibility','hidden');
-			$('#sf-text').trigger('keyup');
-		});
-	});
 });
 
-jQuery(document).ready(function($){
-	$('#title').keyup(function() {
-		//if ( 0 == $('#sf-text').val().length || $('#title').val().search( $('#sf-text').val() ) === 0 ) {
-			$('#sf-text').val( $('#title').val() );
-			$('#sf-text').trigger('keyup');
-		//}
-	});
 
-  $('#sf-text').maxlength({
-    events: [],				// Array of events to be triggerd    
-    maxCharacters: sf_l10n['max'],		// Characters limit   
-    statusID: "count",
-    status: true,			// True to show status indicator bewlow the element    
-    statusClass: "",	// The class on the status div  
-    statusText: "", 					// The status text  
-    notificationClass: "notification",	// Will be added when maxlength is reached  
-    showAlert: false, // True to show a regular alert message    
-    alertText: "You have typed too many characters.", // Text in alert message   
-    slider: false // True Use counter slider    
-  }); 
-});
