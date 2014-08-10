720344b48f9cbdf6cd930f04396de49d21c73d4c
dizzain <dizzain@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-10 09:17:52 +0000

    Prevent all html escaping for error messages
    git-svn-id: https://plugins.svn.wordpress.org/socialflow/trunk@963220 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- views/form/messages.php
+++ views/form/messages.php
@@ -9,7 +9,7 @@
 if ( is_wp_error( $errors ) ) : ?>
 <div class="socialflow-messages">
 	<?php foreach ( $errors->get_error_messages() as $message ): ?>
-		<p class="sf-error"><?php echo esc_html( $message ); ?></p>
+		<p class="sf-error"><?php echo wp_kses_post( $message ); ?></p>
 	<?php endforeach ?>
 </div><!-- .socialflow-messages -->
 <?php endif; ?>
